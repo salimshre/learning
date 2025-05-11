f6bd04985243e606b0d3b21885ccd1612fca3551
Salim Shrestha <salim9shrestha@gmail.com>
2025-05-11 18:11:58 +0545

    changes

---
+++ Header Files in c++/main.cpp
@@ -0,0 +1,12 @@
+// https://www.youtube.com/watch?v=0TCh01BBlrM&t=13s
+// 
+#include <iostream>
+#include "includes/calculation.h"
+
+using namespace std;
+
+int main(){
+	cout<< add(10,20) <<endl;
+
+	return 0;
+}
